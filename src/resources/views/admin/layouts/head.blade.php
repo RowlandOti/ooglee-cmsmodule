36ec538b7a645348ee1faa0f65da8111cbcfa43b
RowlandOti <rowlandotienoo@gmail.com>
2016-04-21 13:15:48 +0300

    fix: update resource assets path

--- src/resources/views/admin/layouts/head.blade.php
+++ src/resources/views/admin/layouts/head.blade.php
@@ -10,18 +10,18 @@
     
     <!--{{ "CSS START" }}-->
     <!-- Bootstrap 3.3.2 -->
-    <link href="{{ asset("/vendor/admin-lte/bootstrap/css/bootstrap.min.css") }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset("/vendor/rowland/ooglee-cms/vendor/admin-lte/bootstrap/css/bootstrap.min.css") }}" rel="stylesheet" type="text/css" />
     <!-- Font Awesome Icons -->
-    <link href="{{ asset("/vendor/font-awesome/css/font-awesome.min.css") }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset("/vendor/rowland/ooglee-cms/vendor/font-awesome/css/font-awesome.min.css") }}" rel="stylesheet" type="text/css" />
     <!-- Ionicons -->
-    <link href="{{ asset("/vendor/ionicons/css/ionicons.min.css") }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset("/vendor/rowland/ooglee-cms/vendor/ionicons/css/ionicons.min.css") }}" rel="stylesheet" type="text/css" />
     <!-- jvectormap -->
-    <link href="{{ asset("/vendor/admin-lte/plugins/jvectormap/jquery-jvectormap-1.2.2.css") }}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset("/vendor/rowland/ooglee-cms/vendor/admin-lte/plugins/jvectormap/jquery-jvectormap-1.2.2.css") }}" rel="stylesheet" type="text/css" />
     <!-- Theme style -->
-    <link href="{{ asset("/vendor/admin-lte/dist/css/AdminLTE.min.css")}}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset("/vendor/rowland/ooglee-cms/vendor/admin-lte/dist/css/AdminLTE.min.css")}}" rel="stylesheet" type="text/css" />
     <!-- AdminLTE Skins. We have chosen the skin-green for this starter page. However, you can choose any other skin. 
     Make sure you apply the skin class to the body tag so the changes take effect.-->
-    <link href="{{ asset("/vendor/admin-lte/dist/css/skins/skin-green.min.css")}}" rel="stylesheet" type="text/css" />
+    <link href="{{ asset("/vendor/rowland/ooglee-cms/vendor/admin-lte/dist/css/skins/skin-green.min.css")}}" rel="stylesheet" type="text/css" />
     <!--{{ "CSS END" }}-->
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
